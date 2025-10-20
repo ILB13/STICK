1cca934ae0b35eee9f2500fcc655a357d7dc73bb
ILB13 <149909095+ILB13@users.noreply.github.com>
2025-10-20 18:28:50 -0400

    Made some changes

--- controllers/cart_controller/cart_controller.py
+++ controllers/cart_controller/cart_controller.py
@@ -6,10 +6,16 @@ timestep = int(robot.getBasicTimeStep())
 # Get the motor
 x_motor = robot.getDevice('x motor')
 
+delay = 1.5  # seconds
+steps_to_wait = int(delay * 1000 / timestep)
+
+for _ in range(steps_to_wait):
+    robot.step(timestep)
+
 # Infinite position = velocity control
 x_motor.setPosition(float('inf'))
-x_motor.setVelocity(0.0)
+x_motor.setVelocity(1)
 
 while robot.step(timestep) != -1:
     # Just move a little in +X as a test
-    x_motor.setVelocity(0.005)
+    x_motor.setVelocity(1)
